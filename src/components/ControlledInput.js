d8f160f423748f61f0a82deac7d255a846aff031
github username <github email address>
2019-06-14 10:49:17 -0400

    testing uncontrolled components

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -6,11 +6,37 @@ export default class ControlledInput extends React.Component {
     lastName: "Henry"
   }
 
+  handleFirstNameChange = event => {
+    this.setState({
+      firstName: event.target.value
+    })
+  }
+
+  handleLastNameChange = event => {
+    this.setState({
+      lastName: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const firstName = event.target.children[0].value
+    const lastName = event.target.children[1].value
+    this.sendFormDataSomewhere({ firstName, lastName })
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" value={this.state.firstName} />
-        <input type="text" name="lastName" value={this.state.lastName} />
+      <form onSubmit={ event => this.handleSubmit(event) }>
+
+        <input type="text" id="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+
+        <input type="text" id="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+
+        <input type="submit" />
+
+
+
       </form>
     )
   }
